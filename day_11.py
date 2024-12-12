128dd6e545ea770f760dac9f8af2cb6c10e74532
James Robinson <james.em.robinson@gmail.com>
2024-12-12 10:34:57 +0000

    :sparkles: Day 11 part 1

---
+++ day_11.py
@@ -0,0 +1,13 @@
+from advent_of_code_2024.stones import StoneLine
+
+def part_one():
+    stone_line = StoneLine("day-11.txt")
+    print("Day 11 part 1:", stone_line.score(25))
+
+def part_two():
+    pass
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
