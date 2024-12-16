bc9991f8130190973e3c2954a989c2f45a10c0ed
James Robinson <james.em.robinson@gmail.com>
2024-12-16 13:49:15 +0000

    :recycle: Use GridVector instead of Direction in LabMaze problem

--- advent_of_code_2024/lab_maze.py
+++ advent_of_code_2024/lab_maze.py
@@ -1,42 +1,26 @@
 import copy
-import enum
 
 import numpy as np
 
 from advent_of_code_2024.array import StrArray2D
 from advent_of_code_2024.data_loaders import load_file_as_array
-from advent_of_code_2024.grid_location import GridLocation
-
-
-class Direction(enum.IntEnum):
-    NORTH = 0
-    EAST = 1
-    SOUTH = 2
-    WEST = 3
+from advent_of_code_2024.grid_location import GridLocation, GridVector, grid_vectors
 
 
 class Guard:
-    def __init__(self, position: GridLocation, direction: Direction) -> None:
+    def __init__(self, position: GridLocation, direction: GridVector) -> None:
         self.position = position
         self.direction = direction
 
     def next_position(self) -> GridLocation:
-        pos_0, pos_1 = self.position.as_tuple()
-        if self.direction == Direction.NORTH:
-            return GridLocation((pos_0 - 1, pos_1))
-        if self.direction == Direction.EAST:
-            return GridLocation((pos_0, pos_1 + 1))
-        if self.direction == Direction.SOUTH:
-            return GridLocation((pos_0 + 1, pos_1))
-        if self.direction == Direction.WEST:
-            return GridLocation((pos_0, pos_1 - 1))
+        return self.position + self.direction
 
     @property
-    def state(self) -> tuple[GridLocation, Direction]:
+    def state(self) -> tuple[GridLocation, GridVector]:
         return (self.position, self.direction)
 
     def turn(self) -> None:
-        self.direction = Direction((self.direction + 1) % len(Direction))
+        self.direction = self.direction.clockwise_90()
 
 
 class LabMaze:
@@ -46,8 +30,8 @@ class LabMaze:
             int(self.array.array.shape[0] - 1),
             int(self.array.array.shape[1] - 1),
         )
-        self.guard = Guard(self.array.find("^")[0], Direction.NORTH)
-        self.visited: set[tuple[GridLocation, Direction]] = set()
+        self.guard = Guard(self.array.find("^")[0], grid_vectors["north"])
+        self.visited: set[tuple[GridLocation, GridVector]] = set()
 
     def step(self) -> bool:
         """Take a step and return whether the guard has left the map."""
