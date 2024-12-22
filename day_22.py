48e20c0a1d5cbb25b255181d5e7a8422382fd70a
James Robinson <james.em.robinson@gmail.com>
2024-12-22 13:51:07 +0000

    :sparkles: Day 22 part 2

--- day_22.py
+++ day_22.py
@@ -6,12 +6,13 @@ from advent_of_code_2024.banana_market import BananaMarket
 def part_one():
     start = time.monotonic()
     market = BananaMarket("day-22.txt")
-    market.generate_secrets(2000)
     print("Day 22 part 1:", market.sum_buyer_secrets(), f"in {time.monotonic() - start:.3f} seconds")
 
 
 def part_two():
-    pass
+    start = time.monotonic()
+    market = BananaMarket("day-22.txt")
+    print("Day 22 part 2:", market.most_bananas(), f"in {time.monotonic() - start:.3f} seconds")
 
 if __name__ == "__main__":
     part_one()
--- tests/test_day_22.py
+++ tests/test_day_22.py
@@ -1,9 +1,8 @@
 from advent_of_code_2024.banana_market import BananaMarket, Buyer
 
 
-def test_part_one():
-    buyer = Buyer(123)
-    buyer.generate_secrets(10)
+def test_part_one_buyer():
+    buyer = Buyer(123, 10)
     assert [buyer.secrets[idx] for idx in range(11)] == [
         123,
         15887950,
@@ -17,8 +16,10 @@ def test_part_one():
         7753432,
         5908254,
     ]
-    market = BananaMarket("day-22.test.txt")
-    market.generate_secrets(2000)
+
+
+def test_part_one_market():
+    market = BananaMarket("day-22.test-0.txt")
     assert [buyer.secrets[2000] for buyer in market.buyers] == [
         8685429,
         4700978,
@@ -26,3 +27,30 @@ def test_part_one():
         8667524,
     ]
     assert market.sum_buyer_secrets() == 37327623
+
+
+def test_part_two_buyer():
+    buyer = Buyer(123, 10)
+    assert [
+        (buyer.secrets[idx], buyer.prices[idx], buyer.price_diffs[idx])
+        for idx in range(10)
+    ] == [
+        (123, 3, None),
+        (15887950, 0, -3),
+        (16495136, 6, 6),
+        (527345, 5, -1),
+        (704524, 4, -1),
+        (1553684, 4, 0),
+        (12683156, 6, 2),
+        (11100544, 4, -2),
+        (12249484, 4, 0),
+        (7753432, 2, -2),
+    ]
+    assert buyer.price_at_sequence((-1, -1, 0, 2)) == 6
+
+
+def test_part_two_market():
+    market = BananaMarket("day-22.test-1.txt")
+    assert market.price_at_sequence((-2, 1, -1, 3)) == 23
+    assert market.best_sequence() == (-2, 1, -1, 3)
+    assert market.most_bananas() == 23
